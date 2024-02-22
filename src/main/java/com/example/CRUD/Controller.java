d2bdccf108fcd2127f4a7cbae32a380bd6d57f68
carmentecchia <carmentecchia@gmail.com>
2024-02-22 10:28:03 +0100

    crud

--- src/main/java/com/example/CRUD/Controller.java
+++ src/main/java/com/example/CRUD/Controller.java
@@ -12,50 +12,42 @@ import java.util.List;
 public class Controller {
     @Autowired
     private CarRepository carRepository;
-    @PostMapping
-    public ResponseEntity<Car> createCar(@RequestBody Car car) {
-        Car savedCar = carRepository.save(car);
-        return new ResponseEntity<>(savedCar, HttpStatus.CREATED);
+    @PostMapping("/add-car")
+    public Car newCar(@RequestBody Car car){
+        return carRepository.saveAndFlush(car);
     }
-
-    @GetMapping
-    public ResponseEntity<List<Car>> getAllCars() {
-        List<Car> cars = carRepository.findAll();
-        return new ResponseEntity<>(cars, HttpStatus.OK);
+    @GetMapping("/all-cars")
+    public List<Car> allCars(){
+        return carRepository.findAll();
     }
-
     @GetMapping("/{id}")
-    public ResponseEntity<Car> getCarById(@PathVariable Long id) {
-        return carRepository.findById(id)
-                .map(car -> new ResponseEntity<>(car, HttpStatus.OK))
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    public Car carById(@PathVariable Long id){
+        Car carFounded = null;
+        if(carRepository.existsById(id)){
+            carFounded = carRepository.getReferenceById(id);
+        }
+        return carFounded;
     }
-
-    @PatchMapping("/{id}")
-    public ResponseEntity<Car> updateCarType(@PathVariable Long id, @RequestParam String type) {
-        return carRepository.findById(id)
-                .map(car -> {
-                    car.setType(type);
-                    carRepository.save(car);
-                    return new ResponseEntity<>(car, HttpStatus.OK);
-                })
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    @PutMapping("/update-type/{id}")
+    public Car updateCar(@PathVariable Long id, @RequestParam String type){
+        Car carUpdated = carById(id);
+        if(carUpdated!=null){
+            carUpdated.setType(type);
+            carRepository.save(carUpdated);
+        }
+        return carUpdated;
     }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteCar(@PathVariable Long id) {
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteCar(@PathVariable Long id) {
         if (carRepository.existsById(id)) {
             carRepository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.OK);
+            return ResponseEntity.ok("Auto eliminata con successo");
         } else {
-            return new ResponseEntity<>(HttpStatus.CONFLICT);
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Auto id " + id + " non trovata");
         }
     }
-
-    @DeleteMapping
-    public ResponseEntity<Void> deleteAllCars() {
+    @DeleteMapping("/delete-all")
+    public void deleteDb(){
         carRepository.deleteAll();
-        return new ResponseEntity<>(HttpStatus.OK);
     }
-
 }
